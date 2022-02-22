a19915d401f312911ec297f405b0e6ea32da6993
AmadoJunior <amadoelrepalol@gmail.com>
2022-02-21 23:30:23 -0500

    Got a working version of the model

---
+++ index.js
@@ -0,0 +1,133 @@
+//Dependencies
+const fs = require('fs');
+const tf = require('@tensorflow/tfjs-node-gpu');
+const { Console } = require('console');
+
+(async () => {
+
+    function getMNISTData(){
+        //Load Data
+        var trainData  = fs.readFileSync(__dirname + '\\MNISTData\\mnist_train.csv', {encoding: "utf8"}).toString().split('\n').slice(1);
+        var testData = fs.readFileSync(__dirname + '\\MNISTData\\mnist_test.csv', {encoding: "utf8"}).toString().split('\n').slice(1);
+        let xTrains = [];
+        let yTrains = [];
+        let xTests = [];
+        let yTests = [];
+
+        // console.log(testData.length);
+        // console.log(trainData.length);
+
+        for(let i = 0; i < trainData.length-1; i++){
+            let values = trainData[i].split(",");
+            //console.log(values.length);
+            yTrains.push(parseInt(values[0]));
+            xTrains[i] = [];
+            for(let j = 1; j < values.length; j++){
+                xTrains[i].push(parseInt(values[j]));
+            }
+        }
+
+        for(let i = 0; i < testData.length-1; i++){
+            let values = testData[i].split(",");
+            //console.log(values.length);
+            yTests.push(parseInt(values[0]));
+            xTests[i] = [];
+            for(let j = 1; j < values.length; j++){
+                xTests[i].push(parseInt(values[j]));
+            }
+        }
+
+        //console.log(xTrains);
+        //console.log(yTrains);
+        return tf.tidy(() => {
+            const xs = tf.tensor2d(xTrains);
+            const ys = tf.oneHot(tf.tensor1d(yTrains).toInt(), 10);
+            const xsTests = tf.tensor2d(xTests);
+            const ysTests = tf.oneHot(tf.tensor1d(yTests).toInt(), 10);
+            return [xs, ys, xsTests, ysTests];
+        });
+        
+    }
+
+
+    async function trainModel(xs, ys, xsTests, ysTests){
+        //Define Vars
+        const model = tf.sequential();
+        const learningRate = .01;
+        const numberOfEpochs = 100;
+        const optimizer = tf.train.adam(learningRate);
+        //console.log(xs.shape);
+        //console.log(ys.shape);
+
+        //Add Layers
+        model.add(tf.layers.dense({
+            units:16, activation: "sigmoid", inputShape: [xs.shape[1]]
+        }));
+        model.add(tf.layers.dense({
+            units:10, activation: "softmax"
+        }));
+        model.compile({
+            optimizer: optimizer,
+            loss: "categoricalCrossentropy",
+            metrics: ["accuracy"]
+        });
+
+        //Train
+        const history = await model.fit(xs, ys, {
+            batchSize: 128,
+            epochs: numberOfEpochs,
+            validationData: [xsTests, ysTests],
+            callbacks: {
+                onEpochEnd: async (epoch, logs) => {
+                    console.log("Epoch: " + epoch + "Logs:" + logs.loss);
+                    console.log("# Tensors: " + tf.memory().numTensors);
+                    await tf.nextFrame();
+                }
+            }
+        });
+
+        //Save Model
+        let save = await model.save('file://myModel1');
+        console.log(save);
+        console.log("Model Saved");
+    }
+
+    async function doPrediction(xsTests, ysTests){
+        //Get Stored Model
+        const model = await tf.loadLayersModel('file://myModel1/model.json');
+        //Get Data Predictions/Test Answers
+        const yPred = await model.predict(xsTests).argMax(-1).dataSync();
+        const yTrue = ysTests.argMax(-1).dataSync();
+        console.log(yPred);
+        console.log(yTrue);
+
+        //Count Correct/Wrong
+        let correct = 0;
+        let wrong = 0;
+        for(let i = 0; i < yTrue.length; i++){
+            if(yTrue.length == yPred.length){
+                if(yTrue[i] == yPred[i]){
+                    correct++;
+                } else {
+                    wrong++;
+                }
+            } else {
+                console.log("Different Lengths Output and Tests");
+                break;
+            }
+        }
+        console.log("Accuracy: " + correct/(correct+wrong));
+        console.log("Correct: " + correct + "Wrong: " + wrong);
+    }
+
+    //Get Data
+    const [xs, ys, xsTests, ysTests] = getMNISTData();
+
+    // console.log(ys.dataSync());
+    // console.log(ysTests.dataSync());
+
+    await trainModel(xs, ys, xsTests, ysTests);
+
+    await doPrediction(xsTests, ysTests);
+
+})();
